08bad8d607e7809d6f411be940520ff8ace8da32
Dali-mp4 <tejdali2008@gmail.com>
2026-07-26 21:09:53 +0100

    connection update

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host=st.secrets["DB_HOST"],
-    port=st.secrets["DB_PORT"],
-    user=st.secrets["DB_USER"],
-    password=st.secrets["DB_PASSWORD"],
-    database=st.secrets["DB_NAME"],
-)
+def get_connection():
+    return mysql.connector.connect(
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"],
+    )
